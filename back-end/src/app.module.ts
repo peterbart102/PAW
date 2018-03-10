0760a463c3d37c2aced36a6169abe1d5e1ee42c7
Rafal Zelek <r.zelek@oberthur.com>
2018-03-10 12:32:59 +0100

    autoformat of the all files

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  components: [],
+    imports: [],
+    controllers: [AppController],
+    components: [],
 })
-export class ApplicationModule {}
+export class ApplicationModule {
+}
