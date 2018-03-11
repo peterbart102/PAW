d1399c82736ca27cbeb8c16019c7d5ea73ca19c3
Rafal Zelek <r.zelek@oberthur.com>
2018-03-11 23:35:36 +0100

    added unique identifier for user entity

--- back-end/src/auth/user.entity.ts
+++ back-end/src/auth/user.entity.ts
@@ -4,7 +4,7 @@ import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
 export class UserEntity extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
-    @Column()
+    @Column({unique: true})
     email: string;
     @Column()
     passwordHash: string;
