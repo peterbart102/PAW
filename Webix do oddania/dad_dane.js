e2a77edbf508ed397d10b5ff913c905931ba9b76
Chrumek <zaubererin@poczta.onet.pl>
2018-06-15 22:01:52 +0200

    Drag & Drop Webix

---
+++ Webix do oddania/dad_dane.js
@@ -0,0 +1,23 @@
+dane_01=[
+            { id:1, zad1:"The Shawshank Redemption_01"},
+            { id:2, zad1:"The Godfather_01"},
+            { id:3, zad1:"The Godfather: Part II_01"},
+            { id:4, zad1:"Pulp fiction_01"}
+
+		];
+		
+dane_02=[
+            { id:1, zad1:"The Shawshank Redemption_02"},
+            { id:2, zad1:"The Godfather_02"},
+            { id:3, zad1:"The Godfather: Part II_02"},
+            { id:4, zad1:"Pulp fiction_02"}
+
+		];
+		
+dane_03=[
+            { id:9, zad1:"The Shawshank Redemption_03"},
+            { id:10, zad1:"The Godfather_03"},
+            { id:11, zad1:"The Godfather: Part II_03"},
+            { id:12, zad1:"Pulp fiction_03"}
+
+		]
